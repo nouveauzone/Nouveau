4d686d68c80d2b253c461a1dee9d54f9bd898065
rudradalwadi <rudradalwadi27@gmail.com>
2026-05-15 21:01:35 +0530

    fixed serverless api

--- api/index.js
+++ api/index.js
@@ -1,3 +1,16 @@
-const app = require("../backend/server");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "API Running Successfully"
+  });
+});
 
 module.exports = app;
