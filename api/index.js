46199b337bcf4f7e957735661d17bc1a651ce54e
rudradalwadi <rudradalwadi27@gmail.com>
2026-05-15 21:09:23 +0530

    removed duplicate vercel config

--- api/index.js
+++ api/index.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/api", (req, res) => {
-  res.status(200).json({
+  return res.status(200).json({
     success: true,
     message: "API Running Successfully"
   });
